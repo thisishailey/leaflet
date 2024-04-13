be10f0f3e9dc4929a70ea4288b081aed56b227bf
Hailey Kim <kfbkhw@gmail.com>
2024-04-13 16:25:52 +0900

    ✨ working on comment area

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -11,33 +11,43 @@ import {
 } from '@/firebase/db/model';
 import type { Timestamp } from 'firebase/firestore';
 import { getElapsedTime } from '@/util/datetime';
-import Paper from '@mui/material/Paper';
-import {
-    Avatar,
-    Box,
-    Button,
-    Grid,
-    IconButton,
-    Stack,
-    Typography,
-} from '@mui/material';
+import { emptyValue } from '@/util/util';
+
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SendIcon from '@mui/icons-material/Send';
 
 export default function Post({ params }: { params: { postId: string } }) {
     const [loadFailed, setLoadFailed] = useState(false);
     const [alert, setAlert] = useState<string>('');
-    const [isLiked, setIsLiked] = useState(false);
-    const [isBookmarked, setIsBookmarked] = useState(false);
+
     const [username, setUsername] = useState<string>('');
     const [profileSrc, setProfileSrc] = useState<string>('');
-    const [content, setContent] = useState<string>('');
+    const [isLiked, setIsLiked] = useState(false);
+    const [isBookmarked, setIsBookmarked] = useState(false);
+
     const [elapsedTime, setElapsedTime] = useState<string>('');
+    const [content, setContent] = useState<string>('');
     const [images, setImages] = useState<string[]>([]);
+    const [comments, setComments] = useState<string[]>([]);
+    const [likes, setLikes] = useState<number>(0);
+
+    const [currentUserName, setCurrentUserName] = useState<string>('');
+    const [currentUserProfile, setCurrentUserProfile] = useState<string>('');
 
     useEffect(() => {
         const loadPost = async () => {
@@ -58,6 +68,10 @@ export default function Post({ params }: { params: { postId: string } }) {
             const timestamp = postData.timestamp as Timestamp;
             const elapsedTime = getElapsedTime(timestamp.toDate());
             setElapsedTime(elapsedTime);
+            const likes = postData.likes || 0;
+            setLikes(likes);
+            const comments = postData.comments || [];
+            setComments(comments);
 
             let imageUrl: string;
             const getUsername = async () => {
@@ -111,84 +125,177 @@ export default function Post({ params }: { params: { postId: string } }) {
         setIsBookmarked(!isBookmarked);
     };
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const comment = data.get('new-comment');
+
+        emptyValue('#new-comment');
+    };
+
     return (
-        <Paper
-            variant="outlined"
-            sx={{ minHeight: 200, p: 2, borderRadius: 4 }}
-        >
-            <Stack
-                direction={'row'}
-                alignItems={'center'}
-                justifyContent={'space-between'}
+        <>
+            <style>{`footer {display: none;} #bottom-action-buttons {display: none;}`}</style>
+            <Paper
+                variant="outlined"
+                sx={{ minHeight: 200, px: 2, py: 1.5, borderRadius: 4 }}
+            >
+                <Stack
+                    direction={'row'}
+                    alignItems={'center'}
+                    justifyContent={'space-between'}
+                >
+                    <Stack
+                        direction={'row'}
+                        spacing={2}
+                        alignItems={{ xs: 'center', md: 'flex-start' }}
+                    >
+                        <Avatar
+                            src={profileSrc}
+                            alt={username}
+                            sx={{ width: 28, height: 28 }}
+                        >
+                            {username}
+                        </Avatar>
+                        <Stack
+                            direction={{ xs: 'column', md: 'row' }}
+                            spacing={{ xs: 0, md: 3 }}
+                            alignItems={'baseline'}
+                        >
+                            <Typography fontSize={18} fontWeight={600}>
+                                {username}
+                            </Typography>
+                            <Typography fontSize={14} color={'primary.main'}>
+                                {elapsedTime}
+                            </Typography>
+                        </Stack>
+                    </Stack>
+                    <Stack direction={'row'} spacing={1}>
+                        <IconButton
+                            size="large"
+                            color={isLiked ? 'error' : 'default'}
+                            onClick={handleLike}
+                        >
+                            {isLiked ? (
+                                <FavoriteIcon />
+                            ) : (
+                                <FavoriteBorderIcon />
+                            )}
+                        </IconButton>
+                        <IconButton
+                            size="large"
+                            color={isBookmarked ? 'info' : 'default'}
+                            onClick={handleBookmark}
+                        >
+                            {isBookmarked ? (
+                                <BookmarkIcon />
+                            ) : (
+                                <BookmarkBorderIcon />
+                            )}
+                        </IconButton>
+                        <IconButton size="large">
+                            <MoreVertIcon />
+                        </IconButton>
+                    </Stack>
+                </Stack>
+                <Divider aria-hidden="true" sx={{ py: 0.5 }} />
+                <Box px={{ xs: 0, md: 2 }} py={{ xs: 2, md: 3 }}>
+                    <div dangerouslySetInnerHTML={{ __html: content }}></div>
+                </Box>
+                <Grid container gap={2} m={1}>
+                    {images.map((image) => (
+                        <Avatar
+                            key={image}
+                            src={image}
+                            variant="square"
+                            sx={{
+                                width: '100%',
+                                maxWidth: 300,
+                                height: '100%',
+                                maxHeight: 300,
+                            }}
+                        />
+                    ))}
+                </Grid>
+            </Paper>
+            <Paper
+                variant="outlined"
+                sx={{ minHeight: 200, mt: 2, p: 2, borderRadius: 4 }}
             >
                 <Stack
                     direction={'row'}
                     spacing={2}
-                    alignItems={{ xs: 'center', md: 'flex-start' }}
+                    divider={
+                        <Divider
+                            orientation="vertical"
+                            aria-hidden="true"
+                            flexItem
+                        />
+                    }
                 >
-                    <Avatar
-                        src={profileSrc}
-                        alt={username}
-                        sx={{ width: 28, height: 28 }}
-                    >
-                        {username}
-                    </Avatar>
-                    <Stack
-                        direction={{ xs: 'column', md: 'row' }}
-                        spacing={{ xs: 0, md: 3 }}
-                        alignItems={'baseline'}
-                    >
-                        <Typography fontSize={18} fontWeight={600}>
-                            {username}
-                        </Typography>
-                        <Typography fontSize={14} color={'primary.main'}>
-                            {elapsedTime}
-                        </Typography>
-                    </Stack>
+                    <Typography>{`댓글 ${comments.length}개`}</Typography>
+                    <Typography>{`좋아요 ${likes}개`}</Typography>
                 </Stack>
-                <Stack direction={'row'} spacing={1}>
-                    <IconButton
-                        size="large"
-                        color={isLiked ? 'error' : 'default'}
-                        onClick={handleLike}
-                    >
-                        {isLiked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-                    </IconButton>
-                    <IconButton
-                        size="large"
-                        color={isBookmarked ? 'info' : 'default'}
-                        onClick={handleBookmark}
-                    >
-                        {isBookmarked ? (
-                            <BookmarkIcon />
-                        ) : (
-                            <BookmarkBorderIcon />
-                        )}
-                    </IconButton>
-                    <IconButton size="large">
-                        <MoreVertIcon />
-                    </IconButton>
-                </Stack>
-            </Stack>
-            <Divider aria-hidden="true" sx={{ py: 1 }} />
-            <Box p={3}>
-                <div dangerouslySetInnerHTML={{ __html: content }}></div>
-            </Box>
-            <Grid container gap={2} m={1}>
-                {images.map((image) => (
-                    <Avatar
-                        key={image}
-                        src={image}
-                        variant="square"
-                        sx={{
-                            width: '100%',
-                            maxWidth: 300,
-                            height: '100%',
-                            maxHeight: 300,
-                        }}
+            </Paper>
+            <Box
+                sx={{
+                    position: 'fixed',
+                    bottom: 20,
+                    left: '50%',
+                    transform: 'translateX(-50%)',
+                    width: '100%',
+                    maxWidth: 950,
+                    p: 2,
+                }}
+            >
+                <Paper
+                    component={'form'}
+                    onSubmit={handleSubmit}
+                    variant="outlined"
+                    sx={{
+                        p: 2,
+                        borderRadius: 4,
+                        borderColor: 'primary.main',
+                        display: 'flex',
+                        gap: 2,
+                    }}
+                >
+                    <TextField
+                        autoFocus
+                        fullWidth
+                        required
+                        multiline
+                        rows={4}
+                        id="new-comment"
+                        name="new-comment"
+                        placeholder="댓글을 작성해 주세요."
+                        InputProps={{ sx: { borderRadius: 3 } }}
                     />
-                ))}
-            </Grid>
-        </Paper>
+                    <Stack
+                        direction={'column'}
+                        spacing={1}
+                        alignItems={'center'}
+                        justifyContent={'space-between'}
+                    >
+                        <Avatar src={currentUserProfile}>
+                            {currentUserName}
+                        </Avatar>
+                        <Button
+                            type="submit"
+                            color="primary"
+                            variant="contained"
+                            sx={{
+                                minWidth: 40,
+                                height: 40,
+                                p: 1,
+                                borderRadius: 3,
+                            }}
+                        >
+                            <SendIcon />
+                        </Button>
+                    </Stack>
+                </Paper>
+            </Box>
+        </>
     );
 }
--- src/firebase/db/model.ts
+++ src/firebase/db/model.ts
@@ -1,36 +1,38 @@
 import { Timestamp } from 'firebase/firestore';
 
-export type Collection = UserCollection | PostCollection;
-export type Data = UserData | PostData;
-export type UpdatableData = UserDataUpdate | PostDataUpdate;
+export type Collection = UserCollection | PostCollection | CommentCollection;
+export type Data = UserData | PostData | CommentData;
+export type UpdatableData = UserDataUpdate | PostDataUpdate | CommentDataUpdate;
 
 // user
 export type UserCollection = 'user';
 export const COLLECTION_USER: UserCollection = 'user';
 
 export interface UserData {
+    email: string; // primary key, no update
+    username: string; // unique
     firstname: string;
     lastname: string;
-    username: string;
-    email: string; // key
-    profileImg?: string;
-    description?: string;
-    followers?: string[];
+    profileImg?: string; // unique
+    bio?: string;
+    follower?: string[];
     following?: string[];
     wishlist?: string[];
+    like?: string[];
     bookmark?: string[];
-    timestamp?: Timestamp;
+    timestamp?: Timestamp; // automatic, no update
 }
 
 export interface UserDataUpdate {
-    firstname?: string;
-    lastname?: string;
-    username?: string;
-    profileImg?: string;
-    description?: string;
-    followers?: string[];
+    username: string; // unique
+    firstname: string;
+    lastname: string;
+    profileImg?: string; // unique
+    bio?: string;
+    follower?: string[];
     following?: string[];
     wishlist?: string[];
+    like?: string[];
     bookmark?: string[];
 }
 
@@ -39,15 +41,35 @@ export type PostCollection = 'post';
 export const COLLECTION_POST: PostCollection = 'post';
 
 export interface PostData {
-    email: string; // user key
+    _id?: string; // primary key, automatic, no update
+    email: string; // foreign key (user), no update
     content: string;
     images?: string[];
-    timestamp?: Timestamp;
+    comments?: string[];
+    likes?: number;
+    timestamp?: Timestamp; // automatic, no update
 }
 
 export interface PostDataUpdate {
     content?: string;
     images?: string[];
+    comments?: string[];
+    likes?: number;
+}
+
+// comment
+export type CommentCollection = 'comment';
+export const COLLECTION_COMMENT: CommentCollection = 'comment';
+
+export interface CommentData {
+    id: string; // primary key, automatic, no update
+    email: string; // foreign key (user), no update
+    content: string;
+    timestamp?: Timestamp; // automatic, no update
+}
+
+export interface CommentDataUpdate {
+    content: string;
 }
 
 // review
