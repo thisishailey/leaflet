3d4c0e4d906703f008f70de25bcd2e8789694248
Hailey Kim <kfbkhw@gmail.com>
2024-04-17 20:25:01 +0900

    🚑 Revert the last commit (local env caused errors, back to NEXT_PUBLIC_ 🥲)

--- src/app/api/books/searchByTerm/route.ts
+++ src/app/api/books/searchByTerm/route.ts
@@ -5,7 +5,7 @@ const ENDPOINT = 'http://www.aladin.co.kr/ttb/api/ItemSearch.aspx';
 
 export async function GET(req: NextRequest) {
     const params = new URLSearchParams({
-        ttbkey: process.env.ALADIN_API_KEY as string,
+        ttbkey: process.env.NEXT_PUBLIC_ALADIN_API_KEY as string,
         Query: decodeURIComponent(req.headers.get('search') as string),
         output: 'js',
         Version: '20131101',
